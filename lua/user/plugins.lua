fba46e6571e0db459f21bcf371aa012661297750
Gorob <gaurabkhatry@gmail.com>
2022-03-19 09:30:16 +0545

    removed spot and edit telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -69,22 +69,6 @@ return packer.startup(function(use)
 
     use 'ahmedkhalf/project.nvim'
     use {
-        'KadoBOT/nvim-spotify', 
-        requires = 'nvim-telescope/telescope.nvim',
-        config = function()
-            local spotify = require'nvim-spotify'
-
-            spotify.setup {
-                -- default opts
-                status = {
-                    update_interval = 10000, -- the interval (ms) to check for what's currently playing
-                    format = '%s %t by %a' -- spotify-tui --format argument
-                }
-            }
-        end,
-        run = 'make'
-    }
-    use {
         'lewis6991/gitsigns.nvim',
         requires = {
             'nvim-lua/plenary.nvim'
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,2 +1,7 @@
+require('telescope').setup{
+    defaults ={
+        file_ignore_patterns = {"node_modules"}
+    }
+}
 vim.api.nvim_set_keymap("n","<leader>ff","<cmd>lua require('telescope.builtin').find_files()<CR>",{noremap=true})
 -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
